da070ccb8c6bf9ea65b3e3c7300200cb8b62ff03
kaguralir <arsonistera@gmail.com>
2021-09-11 15:34:19 +0200

    many changes done

--- 23.03cinqpages/PFL numero un/PFL-22-03/pfl/autre.js
+++ 23.03cinqpages/PFL numero un/PFL-22-03/pfl/autre.js
@@ -14,7 +14,7 @@ const face2 = document.getElementById("face2");
 
 const currentYear = new Date().getFullYear();
 
-const newYearTime = new Date(`April 10 ${currentYear} 00:00:00`);
+const newYearTime = new Date(`October 18 ${currentYear} 00:00:00`);
 
 function updateCountDown() {
    const currentTime = new Date();
@@ -89,128 +89,128 @@ fireWorkBtn2.addEventListener("click", () => {
 
 (function ($) {
    function FollowCursor(config) {
-       this.init(config);
+      this.init(config);
    }
 
    FollowCursor.prototype = {
-       middleX : 0,
-       middleY : 0,
+      middleX: 0,
+      middleY: 0,
 
-       //----------------------- protected properties and methods -------------
-       /**
-        * @protected
-        */
-       constructor: FollowCursor,
+      //----------------------- protected properties and methods -------------
+      /**
+       * @protected
+       */
+      constructor: FollowCursor,
 
-       /**
-        * Container element. Should be passed into constructor config
-        * @protected
-        * @type {jQuery}
-        */
-       el: null,
+      /**
+       * Container element. Should be passed into constructor config
+       * @protected
+       * @type {jQuery}
+       */
+      el: null,
 
-       /**
-        * Init/re-init the object
-        * @param {object} config - Config
-        */
-       init: function(config) {
-           $.extend(this, config);
-       },
-       
-       handleMove: function(event) {
-           var newY = 0;
-           var newX = 0;
-           var middleX = this.middleX;
-           var middleY = this.middleY;
-           var degrees = 0;
+      /**
+       * Init/re-init the object
+       * @param {object} config - Config
+       */
+      init: function (config) {
+         $.extend(this, config);
+      },
 
-           /* done together in just four quadrants. */
-           if (event.pageY < middleY && event.pageX < middleX) {
-             /* upper left. */
-             newY = middleY - event.pageY;
-             newX = middleX - event.pageX;
+      handleMove: function (event) {
+         var newY = 0;
+         var newX = 0;
+         var middleX = this.middleX;
+         var middleY = this.middleY;
+         var degrees = 0;
 
-             var radians = Math.atan2(newY, newX);
-             degrees = radians * (180 / Math.PI);
-             degrees = 90 - degrees;
-             degrees *= -1;
-           } else if (event.pageY < middleY && event.pageX > middleX) {
-             /* upper right. */
-             newY = middleY - event.pageY;
-             newX = event.pageX - middleX;
-             add = 0.25 * 360;
+         /* done together in just four quadrants. */
+         if (event.pageY < middleY && event.pageX < middleX) {
+            /* upper left. */
+            newY = middleY - event.pageY;
+            newX = middleX - event.pageX;
 
-             var radians = Math.atan2(newY, newX);
-             degrees = radians * (180 / Math.PI);
-             degrees = 90 - degrees;
-           } else if (event.pageY > middleY && event.pageX > middleX) {
-             /* lower right. */
-             newY = event.pageY - middleY;
-             newX = event.pageX - middleX;
-             add = 0.5 * 360;
+            var radians = Math.atan2(newY, newX);
+            degrees = radians * (180 / Math.PI);
+            degrees = 90 - degrees;
+            degrees *= -1;
+         } else if (event.pageY < middleY && event.pageX > middleX) {
+            /* upper right. */
+            newY = middleY - event.pageY;
+            newX = event.pageX - middleX;
+            add = 0.25 * 360;
 
-             var radians = Math.atan2(newY, newX);
-             degrees = radians * (180 / Math.PI);
-             degrees += 90;
-           } else if (event.pageY > middleY && event.pageX < middleX) {
-             /* lower left. */
-             newY = event.pageY - middleY;
-             newX = middleX - event.pageX;
-             add = 0.75 * 360;
+            var radians = Math.atan2(newY, newX);
+            degrees = radians * (180 / Math.PI);
+            degrees = 90 - degrees;
+         } else if (event.pageY > middleY && event.pageX > middleX) {
+            /* lower right. */
+            newY = event.pageY - middleY;
+            newX = event.pageX - middleX;
+            add = 0.5 * 360;
 
-             var radians = Math.atan2(newY, newX);
-             degrees = radians * (180 / Math.PI);
-             degrees += 90;
-             degrees *= -1;
-           } else if (event.pageX < middleX && event.pageY == middleY) {
-             /* west */
-             degrees = -90;
-           } else if (event.pageX == middleX && event.pageY > middleY) {
-             /* south */              
-             degrees = -180;
-           } else if (event.pageX > middleX && event.pageY == middleY) {
-             /* east */              
-             degrees = 90;
-           }
+            var radians = Math.atan2(newY, newX);
+            degrees = radians * (180 / Math.PI);
+            degrees += 90;
+         } else if (event.pageY > middleY && event.pageX < middleX) {
+            /* lower left. */
+            newY = event.pageY - middleY;
+            newX = middleX - event.pageX;
+            add = 0.75 * 360;
 
-           var $cvs = this.el;
-           // the degrees in this transform are absolute!
-           $cvs.css('-ms-transform', 'rotate(' + degrees + 'deg)');
-           $cvs.css('-o-transform', 'rotate(' + degrees + 'deg)');
-           $cvs.css('-webkit-transform', 'rotate(' + degrees + 'deg)');
-           $cvs.css('-moz-transform', 'rotate(' + degrees + 'deg)');
-       }
+            var radians = Math.atan2(newY, newX);
+            degrees = radians * (180 / Math.PI);
+            degrees += 90;
+            degrees *= -1;
+         } else if (event.pageX < middleX && event.pageY == middleY) {
+            /* west */
+            degrees = -90;
+         } else if (event.pageX == middleX && event.pageY > middleY) {
+            /* south */
+            degrees = -180;
+         } else if (event.pageX > middleX && event.pageY == middleY) {
+            /* east */
+            degrees = 90;
+         }
+
+         var $cvs = this.el;
+         // the degrees in this transform are absolute!
+         $cvs.css('-ms-transform', 'rotate(' + degrees + 'deg)');
+         $cvs.css('-o-transform', 'rotate(' + degrees + 'deg)');
+         $cvs.css('-webkit-transform', 'rotate(' + degrees + 'deg)');
+         $cvs.css('-moz-transform', 'rotate(' + degrees + 'deg)');
+      }
    }
-   
+
    //----------------------- Initiating jQuery plugin -------------------------
 
    /**
     * Set an element to rotate following the mouse movement.
     */
-   $.fn.followCursor = function() {
-       var dataName = 'followCursor';
+   $.fn.followCursor = function () {
+      var dataName = 'followCursor';
 
-       return this.each(function() {
-           var el = $(this);
-           var initialConfig = $.extend({}, el.data());
-           config = $.extend(initialConfig, {});
-           config.el = el;
-           var instance = new FollowCursor(config);
+      return this.each(function () {
+         var el = $(this);
+         var initialConfig = $.extend({}, el.data());
+         config = $.extend(initialConfig, {});
+         config.el = el;
+         var instance = new FollowCursor(config);
 
-           var position = el.offset();
-           var realWidth = el.outerWidth();
-           var realHeight = el.outerHeight();
-           var middleX = position.left + (el.width() / 2);
-           var middleY = position.top + (el.height() / 2);
+         var position = el.offset();
+         var realWidth = el.outerWidth();
+         var realHeight = el.outerHeight();
+         var middleX = position.left + (el.width() / 2);
+         var middleY = position.top + (el.height() / 2);
 
-           instance.middleX = middleX;
-           instance.middleY = middleY;
+         instance.middleX = middleX;
+         instance.middleY = middleY;
 
-           el.data(dataName, instance);
+         el.data(dataName, instance);
 
-           $(document).mousemove(function(event) {
-               instance.handleMove(event);
-           });
-       });
+         $(document).mousemove(function (event) {
+            instance.handleMove(event);
+         });
+      });
    };
 }(jQuery));
